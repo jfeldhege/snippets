bf19b3a62f2b3e663f57a81f48d605bd51bb7970
jfeldhege <jfeldhege+git@gmail.com>
2024-01-01 16:32:34 +0100

    🐛 Bug fix

--- setup.R
+++ setup.R
@@ -1,4 +1,5 @@
-# Get snippets from github gists ------------------------------------------
+
+# Setup -------------------------------------------------------------------
 
 #install.packages("gistr")
 
@@ -6,6 +7,8 @@ library(gistr)
 library(snippr)
 
 r_snippets_path <- usethis:::rstudio_config_path("snippets", "r.snippets")
+
+# Get snippets from github gists ------------------------------------------
 
 #https://gist.github.com/slopp/27903daaf9fbd41afc73442b600a7618
 slopp_snippets <- gist("27903daaf9fbd41afc73442b600a7618")
@@ -15,9 +18,17 @@ snips <- snippr:::snippets_parse(slopp_snippets$files$r.snippets$content)
 snippet_add(name = "gg", snips$gg, language = "r", path = r_snippets_path)
 
 
-
 # Edit snippets file and save to project directory ------------------------
-
 
 usethis::edit_rstudio_snippets(type = "r")
 
+readLines(r_snippets_path)
+
+
+# Add snippets from this repo to RStudio ----------------------------------
+
+repo_snippets <- snippr:::snippets_parse(readLines("r.snippets"))
+
+purrr::imap(repo_snippets, function(x, y) {
+  snippet_add(name = y, text = x, language = "r", path = r_snippets_path)
+})
